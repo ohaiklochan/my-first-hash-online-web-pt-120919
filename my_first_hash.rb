635fa670650bc78c0af586e2ec5dada16937bd80
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 02:16:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,6 +12,7 @@ def retrieval
 end
 
 def adding
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
   shipping_manifest
